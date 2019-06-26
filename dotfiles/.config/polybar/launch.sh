5a4891ab5dc4a273906b42efe94c9e04f7f45ded
j <jb2483@cornell.edu>
2019-06-26 16:47:08 -0400

    various

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main
-#polybar bar2 &
+polybar main &
+polybar sub1 &
 
 echo "Bars launched..."
