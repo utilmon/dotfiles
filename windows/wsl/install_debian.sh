ad5c9fbb695f70abc06ab95f179e09dac2b71040
Jai <jb2483@cornell.edu>
2020-01-23 21:17:18 -0800

    install build-essential

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install curl wget git zsh
+sudo apt install curl wget git build-essential zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
