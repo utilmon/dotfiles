7affafc8cc94267c0930bb3b5b9ab70a673de40d
Jai <jb2483@cornell.edu>
2020-01-27 01:58:14 -0500

    zsh security

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -6,4 +6,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#compaudit | xargs chmod g-w,o-w
 #sudo apt install neofetch
