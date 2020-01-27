3f9071bf6694709e641ae8c71c28fdf618f9302e
Jai <jb2483@cornell.edu>
2020-01-27 15:40:55 -0500

    git config store

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -2,6 +2,7 @@
 sudo cp ./etc/apt/* /etc/apt/
 sudo apt update && sudo apt full-upgrade
 sudo apt install vim curl wget git build-essential zsh
+git config credential.helper store
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
