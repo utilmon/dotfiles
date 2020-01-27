4052a7b321f09e45544156022aeb34ef75a31299
Jai <jb2483@cornell.edu>
2020-01-27 01:45:44 -0500

    install vim

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp ./etc/apt/* /etc/apt/
 sudo apt update && sudo apt full-upgrade
-sudo apt install curl wget git build-essential zsh
+sudo apt install vim curl wget git build-essential zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
