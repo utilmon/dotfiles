cf5d05e58fbac609da57b2c635f18fe27b208347
Jai <jb2483@cornell.edu>
2020-01-27 20:00:10 -0500

    fix

--- windows/wsl/install_debian2.sh
+++ windows/wsl/install_debian2.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-cp .zshrc .vimrc .p10k.zsh
+cp .zshrc .vimrc .p10k.zsh ~/
 #compaudit | xargs chmod g-w,o-w
 #sudo apt install neofetch
