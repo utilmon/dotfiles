4c317bf68993704c81c64489e826c2eef4be3940
Jai <jb2483@cornell.edu>
2020-01-28 10:29:47 -0500

    neofetch

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp ./etc/apt/* /etc/apt/
 sudo apt update && sudo apt full-upgrade
-sudo apt install vim curl wget git build-essential zsh
+sudo apt install vim curl wget git build-essential neofetch zsh
 git config credential.helper store
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -9,4 +9,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 cp .zshrc .vimrc .p10k.zsh ~/
 #compaudit | xargs chmod g-w,o-w
-#sudo apt install neofetch
