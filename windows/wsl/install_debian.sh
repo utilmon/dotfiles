44cad4e9a92759e8be088ec34adcc7066a79226f
Jai <jb2483@cornell.edu>
2020-01-28 20:44:51 -0500

    updates

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -3,6 +3,8 @@ sudo cp ./etc/apt/* /etc/apt/
 sudo apt update && sudo apt full-upgrade
 sudo apt install vim curl wget git build-essential neofetch zsh
 git config credential.helper store
+git config --global user.email "jb2483@cornell.edu"
+git config --global user.name "Jai"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
