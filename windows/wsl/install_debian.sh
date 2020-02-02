5048ac797bc5f950a3937f3d059a5d5eb0d8a483
Jai <jb2483@cornell.edu>
2020-02-02 09:49:33 -0500

    permission powerlevel10k install

--- windows/wsl/install_debian.sh
+++ windows/wsl/install_debian.sh
@@ -8,6 +8,6 @@ git config --global user.name "Jai"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 cp .zshrc .vimrc .p10k.zsh ~/
 #compaudit | xargs chmod g-w,o-w
