1518d890c6d0dfec1e1583c23cba580ee9f47edd
Jai <jb2483@cornell.edu>
2021-04-20 21:29:24 -0400

    automatic year tag label

--- misc scripts/youtube music/upload_ytmusic.py
+++ misc scripts/youtube music/upload_ytmusic.py
@@ -4,23 +4,34 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 from ytmusicapi import YTMusic
+import music_tag
+from datetime import date
 
-directories = ['D:\Kwan\Desktop', 'D:\Kwan\내 음악']
+directories = ["D:\Kwan\Desktop", "D:\Kwan\내 음악"]
 # torrent folder is not allowed due to slow download (> 60 s)
-ytmusic = YTMusic('ytmusic_auth.json')  # Authentication file
-filetypes = ['.mp3', 'flac', '.wma', '.m4a', '.ogg']  # only last four elements
+ytmusic = YTMusic("ytmusic_auth.json")  # Authentication file
+filetypes = [".mp3", "flac", ".wma", ".m4a", ".ogg"]  # only last four elements
+
+
+def set_year_tag(fn):
+    """ This function sets year tag if empty """
+    f = music_tag.load_file(fn)
+    if f["year"].value == 0:
+        f["year"] = int(date.today().strftime("%Y"))
+        f.save()
 
 
 def on_created(event):
     """ This function gets executed when a file is created in directories being monitored """
     fn = event.src_path
-    print(f'fn is {fn} and extension is {fn[-4:]}')
+    print(f"fn is {fn} and extension is {fn[-4:]}")
     if fn[-4:] in filetypes:
         time.sleep(30)  # Wait until download is done
         try:
+            set_year_tag(fn)
             ytmusic.upload_song(fn)
         except:
-            print('File does not exist')
+            print("File does not exist")
             pass
 
 
@@ -30,7 +41,9 @@ if __name__ == "__main__":
     ignore_patterns = ""
     ignore_directories = False
     case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler = PatternMatchingEventHandler(
+        patterns, ignore_patterns, ignore_directories, case_sensitive
+    )
 
     my_event_handler.on_created = on_created
 
