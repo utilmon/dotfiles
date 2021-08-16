8931a59fdd53f0add20992d410b359628bf77e5d
EC2 Default User <ec2-user@ip-172-31-36-48.us-east-2.compute.internal>
2021-08-16 01:23:03 +0000

    update

--- amazon-linux/install_al.sh
+++ amazon-linux/install_al.sh
@@ -3,3 +3,6 @@ yum install zsh git tmux util-linux-user
 amazon-linux-extras install docker
 service docker start
 usermod -a -G docker ec2-user
+cp .zshrc .alias .vimrc .tmux.conf ~/
+chsh -s $(which zsh)
+zsh
