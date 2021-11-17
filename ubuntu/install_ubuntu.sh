311b61ce726c25e1fbd2d995be05195d478b4d68
Jai <jb2483@cornell.edu>
2021-11-17 14:55:34 -0500

    update

--- ubuntu/install_ubuntu.sh
+++ ubuntu/install_ubuntu.sh
@@ -5,14 +5,14 @@ sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-stable
 sudo add-apt-repository ppa:tatokis/ckb-next
 
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install vim curl wget git source-highlight neofetch zsh man zsh-antigen fasd tmux -y
+sudo apt install vim curl wget git source-highlight neofetch zsh man zsh-antigen fasd tmux apt-transport-https -y
 #sudo apt install build-essential gdb -y
 ## c language formatting in vscode
 #sudo apt install libtinfo5 -y
 
 
 # mpv, fsearch
-sudo apt install mpv fsearch-trunk zathura keepassxc autokey-gtk fonts-noto-cjk kitty ckb-next pulseeffects -y
+sudo apt install mpv fsearch-trunk zathura keepassxc autokey-gtk fonts-noto-cjk kitty ckb-next pulseeffects gnome-tweaks imwheel -y
 
 # texlive
 #sudo apt install texlive-full -y
@@ -25,5 +25,47 @@ git config credential.helper store
 git config --global user.email "jb2483@cornell.edu"
 git config --global user.name "Jai"
 cp .zshrc .vimrc .tmux.conf .alias ~/
+#cp -r .config ~/
+
+# xkb
+cp ./xkb/symbols/us /usr/share/X11/xkb/symbols/
+
+## custom programs
+
+cd ~/Downloads
+
+# firefox
+sudo snap remove firefox
+rm -r ~/snap/firefox
+
+sudo apt install firefox -y
+
+# Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# VScode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt update
+sudo apt install code -y # or code-insiders
+
+# Orchis-theme
+git clone https://github.com/vinceliuice/Orchis-theme.git
+./Orchis-theme/install.sh
+
+# mamba forge
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+#sudo bash ./Mambaforge-Linux-x86_64.sh
+
 chsh -s $(which zsh) # make zsh default shell
 zsh
