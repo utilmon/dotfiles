de0b3d2e3b13b970997e855a114350c94393387d
JaiKwan Bae <j@JaiKwans-MacBook-Air.local>
2022-08-10 10:52:33 -0400

    added

--- mac/install_mac.sh
+++ mac/install_mac.sh
@@ -1,1 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install source-highlight antigen neofetch
+mkdir ~/.ssh && cp ./.ssh/config ~/.ssh/ 
+#chmod 600 ~/.ssh/config
+cp .zshrc .alias .vimrc .p10k.zsh ~/
