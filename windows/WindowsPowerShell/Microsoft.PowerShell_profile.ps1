478e1ac572e03ea4e87e5067c3d8a89663ff6b11
Jai Kwan Bae <jb2483@cornell.edu>
2024-01-24 03:06:45 +0000

    oh-my-posh

---
+++ windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+oh-my-posh init pwsh --config 'C:\Users\jkwan.bae\Documents\WindowsPowerShell\.mytheme.omp.json' | Invoke-Expression
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name py -Value python
