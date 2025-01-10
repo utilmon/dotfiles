89731620365e8c8615cbc0ccf56a733bc415d6e1
utilmon <87019799+utilmon@users.noreply.github.com>
2025-01-10 14:43:57 +0900

    Update Microsoft.PowerShell_profile.ps1
    use open command

--- windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 oh-my-posh init pwsh --config 'C:\Users\jkwan.bae\Documents\WindowsPowerShell\.mytheme.omp.json' | Invoke-Expression
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name py -Value python
+Set-Alias -Name open -Value Explorer
