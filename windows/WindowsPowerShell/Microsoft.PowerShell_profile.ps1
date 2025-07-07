cb159d77c87f9eacc9af01bbe38a6188b6028260
utilmon <utilmonn@gmail.com>
2025-07-07 22:12:56 +0900

    up

--- windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,15 @@
-oh-my-posh init pwsh --config 'C:\Users\jkwan.bae\Documents\WindowsPowerShell\.mytheme.omp.json' | Invoke-Expression
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name py -Value python
 Set-Alias -Name open -Value Explorer
+oh-my-posh init pwsh --config "C:\Users\qwane\Documents\WindowsPowerShell\mytheme.omp.json" | Invoke-Expression
+
+Import-Module PSReadLine
+Import-Module syntax-highlighting
+
+# Make the 'End' key accept the PSReadLine inline suggestion (autocomplete).
+Set-PSReadLineKeyHandler -Chord "End" -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord "Ctrl+Spacebar" -Function Forwardword
+
+## Shows navigable menu of all options when hitting Tab
+#Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
